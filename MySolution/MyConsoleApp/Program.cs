34a00db3cb84e5bbdc695b149ffbe1561c6dae02
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-01 10:06:48 -0300

    :bulb: docs: Comentários de Código Fonte, aula 08

---
+++ MySolution/MyConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MyConsoleApp
+{
+	internal class Program
+	{
+		/*
+			Esta é uma aplicação de teste, sendo usada aénas para fins de teste.
+			Onde estamos testando linhas e blocos de comentários
+		*/
+		static void Main(string[] args)
+		{
+			//Escreve a mensagem Primeiro Comando
+			Console.WriteLine("Primeiro oComando!");
+			Console.WriteLine("Segundo Comando");
+			Console.WriteLine("Terceiro Comando!");
+			Console.WriteLine("Quarto Comando");
+		}
+	}
+}
