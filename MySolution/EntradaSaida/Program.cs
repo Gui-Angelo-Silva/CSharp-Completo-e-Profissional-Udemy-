25fa7c98ad3973637ba365a041d9750fbbd994a8
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-01 11:38:45 -0300

    :sparkles: feat: Adicionando métodos de entradas e saidas, aula 21

---
+++ MySolution/EntradaSaida/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntradaSaida
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//Console.WriteLine("Guilherme");
+			//Console.WriteLine("Silva");
+
+			//int codigo = Console.Read()
+			//Console.WriteLine(codigo);
+
+			string texto = Console.ReadLine();
+			Console.WriteLine(texto);
+
+			Console.ReadKey();
+		}
+	}
+}
