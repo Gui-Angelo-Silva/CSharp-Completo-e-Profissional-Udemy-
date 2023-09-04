ed9f1d6cf060fcf3445e7c331819bb459b6c44ea
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-04 11:07:51 -0300

    :sparkles: feat: Adicionando Exercício Vetor - Array unidimensional, aula 36

---
+++ Colecoes/Colecao/Colecao/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecao
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int numero = 0;
+
+			int[] numeros = new int[5];
+
+			numeros[0] = 10;
+			numeros[1] = 20;
+			numeros[2] = 30;
+			numeros[3] = 40;
+			numeros[4] = 50;
+
+			numeros[2] = 55;
+
+			string[] nomes =
+			{
+				"Guilherme",
+				"Gabriel",
+				"Gustavo",
+			};
+
+			Console.WriteLine(numeros[2]);
+			Console.WriteLine(nomes[0]);
+			Console.ReadKey();
+		}
+}
+}
