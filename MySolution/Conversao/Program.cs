a3250c902eca3eb98ef6004e306c7fc0a3c98aa5
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-04 09:08:06 -0300

    :sparkles: feat: Adicionando exercício de conversão explícita de tipos numericos, aula 24

--- MySolution/Conversao/Program.cs
+++ MySolution/Conversao/Program.cs
@@ -10,18 +10,30 @@ namespace Conversao
 	{
 		static void Main(string[] args)
 		{
-			byte num1 = 100; // 8 bits, de 0 a 255
-			ushort num2; // 16 bits, 0 a 65.535
+			#region Conversão Implícita
+			//byte num1 = 100; // 8 bits, de 0 a 255
+			//ushort num2; // 16 bits, 0 a 65.535
 
-			float num3  = 1250.45f;
-			double num4 = num3;
+			//float num3  = 1250.45f;
+			//double num4 = num3;
 
-			num3 = num1;
-			num2 = num1;
+			//num3 = num1;
+			//num2 = num1;
 
-			int numero = 'C';
+			//int numero = 'C';
+			#endregion
 
-			Console.WriteLine(numero);
+			#region Conversão Explícita
+			//ushort num1 = 500;
+			//byte num2 = (byte)num1;
+
+			//float num3 = 2500f;
+			//int num4 = (int)num3;
+
+			//char letra = (char)97;
+			#endregion
+
+			Console.WriteLine();
 			Console.ReadKey();
 		}
 	}
