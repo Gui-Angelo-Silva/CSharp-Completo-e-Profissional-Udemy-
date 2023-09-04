721a76600aa21b78d5075cf91f6820778444ca00
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-04 11:51:07 -0300

    :sparkles: feat: Adicionando Estruturas Condicionais Aninhadas, aula 42

--- Condicionais/Condicao/Program.cs
+++ Condicionais/Condicao/Program.cs
@@ -25,26 +25,48 @@ namespace Condicao
 			//Console.ReadKey();
 			#endregion
 
-			int valor = 15;
+			#region Condicionais Encadeadas
+			//int valor = 25;
 
-			if (valor < 5)
-			{
-				Console.WriteLine("Condição verdadeira");
-			}
-			else if (valor >= 5 && valor < 10)
-			{
-				Console.WriteLine("Condição alternativa 1");
-			}
-			else if (valor >= 10 && valor < 20)
-			{
-				Console.WriteLine("Condição alternativa 2");
-			}
-			else 
-			{ 
-				Console.WriteLine("Condição alternativa final"); 
-			}
+			//if (valor < 5)
+			//{
+			//	Console.WriteLine("Condição verdadeira");
+			//}
+			//else if (valor >= 5 && valor < 10)
+			//{
+			//	Console.WriteLine("Condição alternativa 1");
+			//}
+			//else if (valor >= 10 && valor < 20)
+			//{
+			//	Console.WriteLine("Condição alternativa 2");
+			//}
+			//else 
+			//{ 
+			//	Console.WriteLine("Condição alternativa final"); 
+			//}
+			#endregion
 
+			#region Condicionais Aninhadas
+			int numero = 10;
+
+			if (numero > 5)
+			{
+				Console.Write("O número é maior que 5");
+
+				if (numero % 2 == 0 )
+				{
+					Console.WriteLine(" é também é par.");
+				}
+				else
+				{
+					Console.WriteLine(" mas não é par.");
+				}
+			}
+			#endregion
+
+			#region Mostrar
 			Console.ReadKey();
+			#endregion
 		}
 	}
 }
