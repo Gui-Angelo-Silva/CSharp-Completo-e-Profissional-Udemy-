a672035471116e98388f8a0033e532c174efdaef
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-04 09:39:40 -0300

    :sparkles: feat: Adicionando exercício sobre operadores aritméticos (matemáticos), aula 27

---
+++ MySolution/Operadores/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int num1 = 15;
+			int num2 = 2;
+			//int resultado = num1 + num2;
+
+			Console.WriteLine(num1 + num2);
+			Console.WriteLine(num1 - num2);
+			Console.WriteLine(num1 * num2);
+			Console.WriteLine(num1 / num2);
+			Console.WriteLine(num1 % num2);
+			
+			Console.ReadKey();
+		}
+	}
+}
