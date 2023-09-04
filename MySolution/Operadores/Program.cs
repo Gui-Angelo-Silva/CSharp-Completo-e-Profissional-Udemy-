993bd5662dd8ece1aa164b9f837c60b5eba88ea5
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-04 09:50:28 -0300

    :sparkles: feat: Adicionando exercício sobre operadores de incremento e decremento, aula 29

--- MySolution/Operadores/Program.cs
+++ MySolution/Operadores/Program.cs
@@ -22,15 +22,29 @@ namespace Operadores
 			//Console.WriteLine(num1 % num2);
 			#endregion
 
-			#region
-			double num1 = 100;
-			double num2 = 10;
-			double num3 = 5;
+			#region Procedência de Operadores
+			//double num1 = 100;
+			//double num2 = 10;
+			//double num3 = 5;
 
-			double res = (num1 + num2) * (num3 / 3);
+			//double res = (num1 + num2) * (num3 / 3);
 			#endregion
 
-			Console.WriteLine(res);
+			#region
+			int num1 = 10;
+
+			//num1 = num1 + 1;
+			//num1++;
+			//++num1;
+			//num1--;
+			//--num1;
+
+			Console.WriteLine(num1++);
+
+			Console.WriteLine(num1);
+			#endregion
+
+			Console.WriteLine();
 			Console.ReadKey();
 		}
 	}
