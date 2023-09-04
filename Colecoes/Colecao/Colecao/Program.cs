663db9c8bf2ca0084d9ddac6d9ec25a14443b113
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-04 11:16:14 -0300

    :sparkles: feat: Adicionando Exercício Vetor - Array bidimensional, aula 37

--- Colecoes/Colecao/Colecao/Program.cs
+++ Colecoes/Colecao/Colecao/Program.cs
@@ -10,28 +10,60 @@ namespace Colecao
 	{
 		static void Main(string[] args)
 		{
-			int numero = 0;
+			#region Vetores
+			//int numero = 0;
 
-			int[] numeros = new int[5];
+			//int[] numeros = new int[5];
 
-			numeros[0] = 10;
-			numeros[1] = 20;
-			numeros[2] = 30;
-			numeros[3] = 40;
-			numeros[4] = 50;
+			//numeros[0] = 10;
+			//numeros[1] = 20;
+			//numeros[2] = 30;
+			//numeros[3] = 40;
+			//numeros[4] = 50;
 
-			numeros[2] = 55;
+			//numeros[2] = 55;
 
-			string[] nomes =
+			//string[] nomes =
+			//{
+			//	"Guilherme",
+			//	"Gabriel",
+			//	"Gustavo",
+			//};
+
+			//Console.WriteLine(numeros[2]);
+			//Console.WriteLine(nomes[0]);
+			#endregion
+
+			int[,] numeros = new int[2, 3];
+
+			numeros[0, 0] = 10;
+			numeros[0, 1] = 20;
+			numeros[0, 2] = 30;
+
+			numeros[1, 0] = 40;
+			numeros[1, 1] = 50;
+			numeros[1, 2] = 60;
+
+			Console.Write("[" + numeros[0,2] + "]");
+			Console.Write("[" + numeros[0,0] + "]");
+			Console.Write("[" + numeros[0,1] + "]");
+
+			Console.WriteLine();
+
+			Console.Write("[" + numeros[1, 0] + "]");
+			Console.Write("[" + numeros[1, 1] + "]");
+			Console.Write("[" + numeros[1, 2] + "]");
+
+			string[,] nomes =
 			{
-				"Guilherme",
-				"Gabriel",
-				"Gustavo",
+				{ "Guilherme", "Gabriel", "Gustavo" },
+				{ "Ana", "Felipe", "João" }
 			};
 
-			Console.WriteLine(numeros[2]);
-			Console.WriteLine(nomes[0]);
+			Console.WriteLine(nomes[0, 1]);
+
+			Console.WriteLine();
 			Console.ReadKey();
 		}
-}
+	}
 }
