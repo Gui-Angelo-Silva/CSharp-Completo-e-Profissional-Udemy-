a632a50ba1d3b22d01c0f5b9e9bdb144f228cbbe
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-05 10:31:28 -0300

    :sparkles: feat: Adicionando Métodos com Parâmetros (POO), aula 57

---
+++ POO/MetodosDasClasses/Metodos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosDasClasses
+{
+	class Metodos
+	{
+		// Métodos Simples
+		public void Cumprimentar()
+		{
+			Console.WriteLine("Olá, seja bem vindo.");
+		}
+
+		//Métodos com parâmetros
+		public void Somar(int num1, int num2)
+		{
+			int resultado = num1 + num2;
+			Console.WriteLine("A soma é: " + resultado);
+		}
+
+		public void Apresentar(string nome, int idade)
+		{
+			Console.WriteLine("Meu nome é: " + nome + " e tenho " + idade + " anos");
+		}
+	}
+}
---
+++ POO/MetodosDasClasses/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosDasClasses
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Metodos m = new Metodos();
+			m.Cumprimentar();
+			m.Somar(10, 5);
+			m.Apresentar("Guilherme", 18);
+
+			Console.ReadKey();
+		}
+	}
+}
