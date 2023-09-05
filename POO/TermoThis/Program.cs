e3d1519edc80434cce007c6ebd268e517b1d0e82
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-05 11:39:17 -0300

    :sparkles: feat: Adicionando Utilizando o termo this (POO), aula 61

---
+++ POO/TermoThis/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TermoThis
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Acessar acessar = new Acessar();
+
+			if (acessar.Login("gui123"))
+			{
+				Console.WriteLine("Seja bem vindo!");
+			}
+			else
+			{
+				Console.WriteLine("Acesso negado!");
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
