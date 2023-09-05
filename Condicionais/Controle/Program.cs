88dbf6571d08bca108de7e2a5a96e5ce108194eb
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-05 08:46:36 -0300

    :sparkles: feat: Adicionando Estrutura de Controle (switch - case), aula 45

---
+++ Condicionais/Controle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controle
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int escolha = 1;
+
+			switch (escolha)
+			{
+				default:
+					Console.WriteLine("Opção Default");
+					break;
+				case 1:
+					Console.WriteLine("Opção 1");
+					break;
+				case 2:
+					Console.WriteLine("Opção 2");
+					break;
+				case 3:
+				case 4:
+					Console.WriteLine("Opção 3");
+					break;
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
