824f832900eb570cd1f78e6c0587ae8aed7b2b1b
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-05 10:03:48 -0300

    :sparkles: feat: Adicionando Classes e Objetos (POO), aula 53

---
+++ POO/ClassesObjetos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesObjetos
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MinhaClasse mClasse = new MinhaClasse();
+			MinhaClasse m2 = null;
+			
+			OutraClasse outra = new OutraClasse();
+			OutraClasse outra2 = outra;
+		}
+	}
+
+	class MinhaClasse
+	{
+
+	}
+}
---
+++ POO/ClassesObjetos/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("ClassesObjetos")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClassesObjetos")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("e8df3c36-959d-46d3-848b-6c1a89afd906")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
