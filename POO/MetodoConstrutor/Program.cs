63fbe979b30476a2970112c49875a6938cd917a4
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-05 21:04:32 -0300

    :sparkles: feat: Adicionando Método construtor da classe (POO), aula 63

---
+++ POO/MetodoConstrutor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoConstrutor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+
+            p1.nome = "Guilherme";
+
+            Console.WriteLine(p1.nome);
+            Console.WriteLine(p1.sobrenome);
+            Console.WriteLine(p1.anoNascimento);
+            Console.WriteLine(p1.idade);
+
+            Console.ReadKey();
+        }
+    }
+}
