9c298d9ee9749e25e87268660885e5a1d9bf49d7
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-05 21:20:13 -0300

    :sparkles: feat: Adicionando Classes Estáticas (POO), aula 65

---
+++ POO/Estaticas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estaticas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matematica.taxa = 10;
+
+            int valor1 = Matematica.Adicionar(200);
+            int valor2 = Matematica.Diminuir(200);
+
+            Console.WriteLine("Valor 1: " + valor1);
+            Console.WriteLine("Valor 2: " + valor2);
+
+            Console.ReadKey();
+        }
+    }
+}
