ada338e16f3b7cd944ca4a3a8eb6b0e2d7467922
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-05 21:27:23 -0300

    :sparkles: feat: Adicionando Membros Estáticos (POO), aula 66

---
+++ POO/Estaticas/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estaticas
+{
+    class Pessoa
+    {
+        public static int maiorIdade = 18;
+
+        public string nome;
+
+        public int idade;
+
+        public void Apresentar()
+        {
+            Console.WriteLine("Olá eu seu " + nome);
+        }
+
+        public static int CalcularIdade(int anoNascimento)
+        {
+            return DateTime.Now.Year - anoNascimento;
+        }
+    }
+}
--- POO/Estaticas/Program.cs
+++ POO/Estaticas/Program.cs
@@ -10,13 +10,25 @@ namespace Estaticas
     {
         static void Main(string[] args)
         {
-            Matematica.taxa = 10;
+            #region Classe Estática
+            //Matematica.taxa = 10;
 
-            int valor1 = Matematica.Adicionar(200);
-            int valor2 = Matematica.Diminuir(200);
+            //int valor1 = Matematica.Adicionar(200);
+            //int valor2 = Matematica.Diminuir(200);
 
-            Console.WriteLine("Valor 1: " + valor1);
-            Console.WriteLine("Valor 2: " + valor2);
+            //Console.WriteLine("Valor 1: " + valor1);
+            //Console.WriteLine("Valor 2: " + valor2);
+            #endregion
+
+            Pessoa.maiorIdade = 21;
+
+            Pessoa p1 = new Pessoa();
+            p1.nome = "Guilherme";
+            p1.idade = Pessoa.CalcularIdade(2005);
+
+            Console.WriteLine(p1.nome);
+            Console.WriteLine(p1.idade);
+            Console.WriteLine(Pessoa.maiorIdade);
 
             Console.ReadKey();
         }
