15cf90a7a16356ea432f85381646b2f9c79885d3
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-11 10:50:35 -0300

    :sparkles: feat: Adicionando Argumentos do Método main (POO), aula 80

---
+++ POO/MetodoMain/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoMain
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//string nome = args[0];
+			string senha = "abc123";
+
+			if (senha != args[1]) 
+			{
+				Console.WriteLine("Acesso Negado !");
+			}
+			else
+			{ 
+				Console.WriteLine("Olá " + args[0]);
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
