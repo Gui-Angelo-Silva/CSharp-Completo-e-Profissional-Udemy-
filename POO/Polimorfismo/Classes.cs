3788cea0f6ce53495cc325403ece306a6d4514b9
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-11 09:26:02 -0300

    :sparkles: feat: Adicionando Polimorfismo (POO), aula 75

---
+++ POO/Polimorfismo/Classes.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+	abstract public class Forma
+	{
+		public int X { get; private set; }
+		public int Y { get; private set; }
+		public int Altura { get; set; }
+		public int Largura { get; set; }
+		public int Raio { get; set; }
+
+		public virtual void Desenhar()
+		{
+			Console.WriteLine("Preparando-se para desenhar");
+		}
+
+		public virtual void Area()
+		{
+			double area = Largura * Altura;
+			Console.WriteLine("Área: " + area);
+		}
+
+	}
+	public class Circulo : Forma
+	{
+		public override void Desenhar()
+		{
+			Console.WriteLine("Desenhando um Círculo");
+			base.Desenhar();
+		}
+
+		public override void Area()
+		{
+			double area = 3.14 * (Raio * Raio);
+			Console.WriteLine("Área Círculo " + area);
+		}
+	}
+	public class Retangulo : Forma
+	{
+		public override void Desenhar()
+		{
+			Console.WriteLine("Desenhando um Retângulo");
+			base.Desenhar();
+		}
+
+		public override void Area()
+		{
+			base.Area();
+		}
+	}
+	public class Triangulo : Forma
+	{
+		public override void Desenhar()
+		{
+			Console.WriteLine("Desenhando um Triângulo");
+			base.Desenhar();
+		}
+
+		public override void Area()
+		{
+			double area = (Largura * Altura) / 2;
+			Console.WriteLine("Área Triângulo " + area);
+		}
+	}
+}
---
+++ POO/Polimorfismo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Forma a = new Forma();
+			Forma b = new Triangulo();
+			Forma c = new Circulo();
+			Forma d = new Retangulo();
+
+			//Console.WriteLine("Forma");
+			//a.Desenhar();
+			Console.WriteLine("Triângulo");
+			b.Desenhar();
+			Console.WriteLine("Círculo");
+			c.Desenhar();
+			Console.WriteLine("Retângulo");
+			d.Desenhar();
+
+			Console.ReadKey();
+		}
+	}
+}
