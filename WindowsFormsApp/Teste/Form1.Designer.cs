72eae096ce4ef76eb7a5bf96bf7f61d3dacda118
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-12 09:32:51 -0300

    :sparkles: feat: Adicionando Propriedades das Forms, aula 83

--- WindowsFormsApp/Teste/Form1.Designer.cs
+++ WindowsFormsApp/Teste/Form1.Designer.cs
@@ -28,13 +28,38 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.btn1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// btn1
+			// 
+			this.btn1.Location = new System.Drawing.Point(241, 155);
+			this.btn1.Name = "btn1";
+			this.btn1.Size = new System.Drawing.Size(282, 115);
+			this.btn1.TabIndex = 0;
+			this.btn1.Text = "Executar";
+			this.btn1.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(784, 461);
+			this.Controls.Add(this.btn1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Location = new System.Drawing.Point(200, 100);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Minha Aplicação";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button btn1;
 	}
 }
 
