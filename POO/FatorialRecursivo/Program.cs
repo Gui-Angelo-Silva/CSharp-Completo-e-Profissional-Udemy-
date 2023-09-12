f2c98f929e443a32c0e01bc97e3ec8f11f3a54c2
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-12 08:10:51 -0300

    :sparkles: feat: Adicionando Exercício - Fatorial de um número utilizando recursividade (POO), aula 81

---
+++ POO/FatorialRecursivo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FatorialRecursivo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Calcula fatorial de qual número? ");
+			int num = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("O fatorial de " + num + " é " + Fatorial(num));
+
+			Console.ReadKey();
+		}
+
+		static int Fatorial(int num)
+		{
+			if (num <= 0)
+			{
+				return 1;
+			}
+
+			return num * Fatorial(num - 1);
+		}
+	}
+}
