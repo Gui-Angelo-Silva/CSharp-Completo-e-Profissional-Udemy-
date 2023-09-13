642f4446ba11535926be37fb776c285883518416
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-13 09:25:05 -0300

    :sparkles: feat: Adicionando Acessando as Propriedades das Forms pelo código-fonte, aula 84

--- WindowsFormsApp/Teste/Form1.cs
+++ WindowsFormsApp/Teste/Form1.cs
@@ -15,11 +15,27 @@ namespace Teste
 		public Form1()
 		{
 			InitializeComponent();
-			this.Text = "Guilherme Silva";
-			this.Size = new Size(400,200);
+			//this.Text = "Guilherme Silva";
+			//this.Size = new Size(400,200);
 		}
 
 		private void Form1_Load(object sender, EventArgs e)
+		{
+			this.Text = "Carregando a Form";
+		}
+
+		private void Form1_Click(object sender, EventArgs e)
+		{
+			this.Text = "Clicando na Form";
+			this.Size = new Size(450, 200);
+		}
+
+		private void MeuMetodo(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
 		{
 
 		}
