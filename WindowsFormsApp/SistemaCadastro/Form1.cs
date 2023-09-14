f5eb2e8f472718f21de003f4090efd366b859f9a
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-14 10:43:50 -0300

    :sparkles: feat: Adicionando Sistema Cadastro - Parte 3, aula 90

--- WindowsFormsApp/SistemaCadastro/Form1.cs
+++ WindowsFormsApp/SistemaCadastro/Form1.cs
@@ -34,8 +34,67 @@ namespace SistemaCadastro
 
 		private void btnCadastrar_Click(object sender, EventArgs e)
 		{
+			int index = -1;
 
-		}
+            foreach (Pessoa pessoa in pessoas)
+            {
+                if (pessoa.Nome == txtNome.Text)
+                {
+                    index = pessoas.IndexOf(pessoa);
+                }
+            }
+
+			if (txtNome.Text == "")
+			{
+				MessageBox.Show("Preencha o campo nome.");
+				txtNome.Focus();
+				return;
+			}
+
+			if (txtTelefone.Text == "")
+			{
+				MessageBox.Show("Preencha o campo telefone.");
+				txtTelefone.Focus();
+				return;
+			}
+
+			char sexo;
+
+            if (radioM.Checked)
+            {
+				sexo = 'M';
+            }
+			else if (radioF.Checked)
+			{
+				sexo = 'F';
+			}
+			else
+			{
+				sexo = 'O';
+			}
+
+			Pessoa p = new Pessoa();
+			p.Nome = txtNome.Text;
+			p.DataNascimento = txtNome.Text;
+			p.EstadoCivil = comboEC.SelectedIndex.ToString();
+			p.Telefone = txtTelefone.Text;
+			p.CasaPropria = checkCasa.Checked;
+			p.Veiculo = checkVeiculo.Checked;
+			p.Sexo = sexo;
+
+			if (index < 0)
+			{
+				pessoas.Add(p);
+			}
+			else
+			{
+				pessoas[index] = p;
+			}
+
+			btnLimpar_Click(btnLimpar, EventArgs.Empty);
+
+			Listar();
+        }
 
 		private void btnExcluir_Click(object sender, EventArgs e)
 		{
