13feca559d6a41efcbeb40853ee7e016aafdee6e
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-14 11:44:43 -0300

    :sparkles: feat: Contains - ToString, aula 93

---
+++ WindowsFormsApp/ManipularString/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ManipularString
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.Executar = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(13, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(775, 280);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "label1";
+			// 
+			// Executar
+			// 
+			this.Executar.Location = new System.Drawing.Point(627, 375);
+			this.Executar.Name = "Executar";
+			this.Executar.Size = new System.Drawing.Size(161, 63);
+			this.Executar.TabIndex = 1;
+			this.Executar.Text = "Executar";
+			this.Executar.UseVisualStyleBackColor = true;
+			this.Executar.Click += new System.EventHandler(this.Executar_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.Executar);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button Executar;
+	}
+}
+
---
+++ WindowsFormsApp/ManipularString/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManipularString
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Executar_Click(object sender, EventArgs e)
+		{
+			//string texto = "Este tipo de variável é alfanumérica.";
+
+			//if (texto.Contains("tipo"))
+			//{
+			//label1.Text = "Contém.";
+			//}
+			//else
+			//{
+			//label1.Text = "Não Contém.";
+			//}
+
+			int num = 5400;
+			bool res = true;
+
+			label1.Text = num.ToString();
+        }
+	}
+}
