56336244716f8629bc1202e8da040d222418d46a
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-25 11:55:15 -0300

    :sparkles: feat: Adicionando Quase pronto, aula 109

--- WindowsFormsApp/ManipularNumeros/Form1.cs
+++ WindowsFormsApp/ManipularNumeros/Form1.cs
@@ -33,17 +33,28 @@ namespace ManipularNumeros
 			//}
 			#endregion
 
+			#region ToString / Convert.ToString
 			//double num = 120.5524;
 			//lblResultado.Text = num.ToString("#.00");
 
-			int num = 254;
+			//int num = 254;
 			//lblResultado.Text = num.ToString("X"); //Hex
 			//lblResultado.Text = num.ToString("D"); // Dec
 			//lblResultado.Text = num.ToString("X2"); // Hex min 2 dígitos
 			//lblResultado.Text = num.ToString("D2"); // Dec min 2 dígitos
 			//lblResultado.Text = num.ToString("C"); // Monetário
 
-			lblResultado.Text = Convert.ToString(num, 8);
+			//lblResultado.Text = Convert.ToString(num, 8);
+			#endregion
+
+			double pi = Math.PI;
+			double E = Math.E;
+			double seno = Math.Sin(3);
+			double cosseno = Math.Cos(3);
+			double potencia = Math.Pow(2, 10);
+			double raizQuadrada = Math.Sqrt
+
+			lblResultado.Text = potencia.ToString();
 		}
 	}
 }
