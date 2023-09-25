0d515d1710d1f78acbd6f43d39d8c3577144d44c
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-25 10:33:00 -0300

    :sparkles: feat: Adicionando Método TryParse, aula 107

---
+++ WindowsFormsApp/ManipularNumeros/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ManipularNumeros
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnExecutar = new System.Windows.Forms.Button();
+			this.lblResultado = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// btnExecutar
+			// 
+			this.btnExecutar.Location = new System.Drawing.Point(601, 381);
+			this.btnExecutar.Name = "btnExecutar";
+			this.btnExecutar.Size = new System.Drawing.Size(187, 57);
+			this.btnExecutar.TabIndex = 0;
+			this.btnExecutar.Text = "Executar";
+			this.btnExecutar.UseVisualStyleBackColor = true;
+			this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+			// 
+			// lblResultado
+			// 
+			this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblResultado.Location = new System.Drawing.Point(13, 13);
+			this.lblResultado.Name = "lblResultado";
+			this.lblResultado.Size = new System.Drawing.Size(775, 356);
+			this.lblResultado.TabIndex = 1;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.lblResultado);
+			this.Controls.Add(this.btnExecutar);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnExecutar;
+		private System.Windows.Forms.Label lblResultado;
+	}
+}
+
---
+++ WindowsFormsApp/ManipularNumeros/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManipularNumeros
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnExecutar_Click(object sender, EventArgs e)
+		{
+			int num;
+			//bool res = int.TryParse("1985", out num);
+
+			if (int.TryParse("1985", out num))
+			{
+				lblResultado.Text = num.ToString();
+			}
+			else
+			{
+				lblResultado.Text = "Erro na conversão";
+			}
+		}
+	}
+}
