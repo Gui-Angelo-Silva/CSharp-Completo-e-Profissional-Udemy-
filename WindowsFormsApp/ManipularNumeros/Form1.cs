b609d560aac6005c953b2f90d185b59589c0ab0c
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-25 10:56:31 -0300

    :sparkles: feat: Adicionando Método ToString, aula 108

--- WindowsFormsApp/ManipularNumeros/Form1.cs
+++ WindowsFormsApp/ManipularNumeros/Form1.cs
@@ -19,17 +19,31 @@ namespace ManipularNumeros
 
 		private void btnExecutar_Click(object sender, EventArgs e)
 		{
-			int num;
-			//bool res = int.TryParse("1985", out num);
+			#region TryParse
+			//int num;
+			////bool res = int.TryParse("1985", out num);
 
-			if (int.TryParse("1985", out num))
-			{
-				lblResultado.Text = num.ToString();
-			}
-			else
-			{
-				lblResultado.Text = "Erro na conversão";
-			}
+			//if (int.TryParse("1985", out num))
+			//{
+			//	lblResultado.Text = num.ToString();
+			//}
+			//else
+			//{
+			//	lblResultado.Text = "Erro na conversão";
+			//}
+			#endregion
+
+			//double num = 120.5524;
+			//lblResultado.Text = num.ToString("#.00");
+
+			int num = 254;
+			//lblResultado.Text = num.ToString("X"); //Hex
+			//lblResultado.Text = num.ToString("D"); // Dec
+			//lblResultado.Text = num.ToString("X2"); // Hex min 2 dígitos
+			//lblResultado.Text = num.ToString("D2"); // Dec min 2 dígitos
+			//lblResultado.Text = num.ToString("C"); // Monetário
+
+			lblResultado.Text = Convert.ToString(num, 8);
 		}
 	}
 }
