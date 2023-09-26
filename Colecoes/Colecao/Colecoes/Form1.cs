40540fff3fb6f3f2dc0c9497643c572dc3a33050
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-26 09:56:07 -0300

    :sparkles: feat: Adicionando List - Parte 2, aula 112

--- Colecoes/Colecao/Colecoes/Form1.cs
+++ Colecoes/Colecao/Colecoes/Form1.cs
@@ -19,6 +19,8 @@ namespace Colecoes
 
 		private void btnList_Click(object sender, EventArgs e)
 		{
+			Lista.Items.Clear();
+
 			// Array
 			string[] nomes = new string[3];
 			nomes[0] = "Guilherme";
@@ -29,12 +31,44 @@ namespace Colecoes
 			List<string> nomes2 = new List<string>();
 
 			nomes2.Add("Eduardo");
-			nomes2.Add("FAbiana");
+			nomes2.Add("Fabiana");
 			nomes2.AddRange(nomes);
+
+			//if (nomes2.Remove("Eduardo"))
+			//{
+			//	MessageBox.Show("Eduardo removido");
+			//}
+			//else
+			//{
+			//	MessageBox.Show("Não foi possível remover.");
+			//}
+
+			//         if (nomes2.Contains("Gustavo"))
+			//         {
+			//	MessageBox.Show("Contém");
+			//         }
+			//else
+			//{
+			//	MessageBox.Show("Não contém");
+			//}
+
+			//MessageBox.Show("Numero de elementos: " + nomes2.Count);
+
+			//nomes2.Sort();
+
+			//MessageBox.Show("Fabiana está no indíce: " + nomes2.IndexOf("Fabiana"));
+
+			//nomes2.Insert(2, "João");
+
+			//nomes2.RemoveAt(1);
+
+			//nomes2.Clear();
+
+			nomes2.Add("Guilherme");
 
             foreach (string nome in nomes2)
             {
-				Lista.Items.Add(nomes);
+				Lista.Items.Add(nome);
 			}
         }
 	}
