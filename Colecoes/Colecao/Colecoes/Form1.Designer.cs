c8f3ae7a29aefe330b4e8a736a7fc4f461b54601
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-09-26 08:57:45 -0300

    :sparkles: feat: Adicionando List - Parte 1, aula 111

--- Colecoes/Colecao/Colecoes/Form1.Designer.cs
+++ Colecoes/Colecao/Colecoes/Form1.Designer.cs
@@ -28,13 +28,49 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.Lista = new System.Windows.Forms.ListBox();
+			this.btnList = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// Lista
+			// 
+			this.Lista.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Lista.FormattingEnabled = true;
+			this.Lista.ItemHeight = 29;
+			this.Lista.Location = new System.Drawing.Point(12, 12);
+			this.Lista.Name = "Lista";
+			this.Lista.Size = new System.Drawing.Size(675, 526);
+			this.Lista.TabIndex = 0;
+			// 
+			// btnList
+			// 
+			this.btnList.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnList.Location = new System.Drawing.Point(693, 12);
+			this.btnList.Name = "btnList";
+			this.btnList.Size = new System.Drawing.Size(172, 55);
+			this.btnList.TabIndex = 1;
+			this.btnList.Text = "List";
+			this.btnList.UseVisualStyleBackColor = true;
+			this.btnList.Click += new System.EventHandler(this.btnList_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(885, 566);
+			this.Controls.Add(this.btnList);
+			this.Controls.Add(this.Lista);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Lista Genéricas";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.ListBox Lista;
+		private System.Windows.Forms.Button btnList;
 	}
 }
 
--- Colecoes/Colecao/Colecoes/Form1.cs
+++ Colecoes/Colecao/Colecoes/Form1.cs
@@ -16,5 +16,26 @@ namespace Colecoes
 		{
 			InitializeComponent();
 		}
+
+		private void btnList_Click(object sender, EventArgs e)
+		{
+			// Array
+			string[] nomes = new string[3];
+			nomes[0] = "Guilherme";
+			nomes[1] = "Ana";
+			nomes[2] = "Gabriel";
+
+			// Lista Genérica
+			List<string> nomes2 = new List<string>();
+
+			nomes2.Add("Eduardo");
+			nomes2.Add("FAbiana");
+			nomes2.AddRange(nomes);
+
+            foreach (string nome in nomes2)
+            {
+				Lista.Items.Add(nomes);
+			}
+        }
 	}
 }
