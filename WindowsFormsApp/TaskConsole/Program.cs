0e40062fdf5339bf3f983b8651f9853589159072
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-10-09 11:21:03 -0300

    :sparkles: feat: Adicionando Task - Parte 1, aula 135

---
+++ WindowsFormsApp/TaskConsole/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Task t = new Task(Tarefa);
+			//t.Start();
+
+			//Task t2 = Task.Run(Tarefa);
+
+			//Task.Run(Tarefa);
+
+			Task.Factory.StartNew(Tarefa);
+
+			Task.Run(() => 
+			{
+				for (int i = 0; i < 10; i++)
+				{
+					Console.WriteLine("Tarefa anônima.");
+				}
+			});
+
+			for (int i = 0; i < 10; i++)
+			{
+				Console.WriteLine("Principal.");
+			}
+
+			Console.ReadKey();
+		}
+
+		private static void Tarefa()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				Console.WriteLine("Tarefa do task.");
+			}
+		}
+	}
+}
