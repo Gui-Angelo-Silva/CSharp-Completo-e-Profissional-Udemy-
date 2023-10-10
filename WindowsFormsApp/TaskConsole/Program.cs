5f932b6078991ae0b1a0026b4720f355f2cf1126
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-10-10 08:43:06 -0300

    :sparkles: feat: Adicionando Task - Parte 2, aula 136

--- WindowsFormsApp/TaskConsole/Program.cs
+++ WindowsFormsApp/TaskConsole/Program.cs
@@ -10,6 +10,7 @@ namespace TaskConsole
 	{
 		static void Main(string[] args)
 		{
+			#region Task Parte 1
 			//Task t = new Task(Tarefa);
 			//t.Start();
 
@@ -17,20 +18,52 @@ namespace TaskConsole
 
 			//Task.Run(Tarefa);
 
-			Task.Factory.StartNew(Tarefa);
+			//Task.Factory.StartNew(Tarefa);
 
-			Task.Run(() => 
-			{
-				for (int i = 0; i < 10; i++)
-				{
-					Console.WriteLine("Tarefa anônima.");
-				}
-			});
+			//Task.Run(() => 
+			//{
+			//	for (int i = 0; i < 10; i++)
+			//	{
+			//		Console.WriteLine("Tarefa anônima.");
+			//	}
+			//});
 
-			for (int i = 0; i < 10; i++)
-			{
-				Console.WriteLine("Principal.");
-			}
+			//for (int i = 0; i < 10; i++)
+			//{
+			//	Console.WriteLine("Principal.");
+			//}
+
+			//Console.ReadKey();
+			#endregion
+
+			//Task[] tasks =
+			//{
+			//	Task.Factory.StartNew(() => 
+			//	{
+			//		Console.WriteLine("Tarefa #1");
+			//	}),
+
+
+			//	Task.Factory.StartNew(() => 
+			//	{
+			//		Console.WriteLine("Tarefa #2");
+			//	}),
+
+			//	Task.Factory.StartNew(() => 
+			//	{
+			//		Console.WriteLine("Tarefa #3");
+			//	})
+			//};
+
+			//Task.WaitAll(tasks);
+
+			Task t1 = Task.Run(() => { Console.WriteLine("Comando #1"); });
+			Task t2 = Task.Run(() => { Console.WriteLine("Comando #2"); });
+			Task t3 = Task.Run(() => { Console.WriteLine("Comando #3"); });
+
+			Task.WaitAll(t1, t2, t3);
+
+			Console.WriteLine("Principal.");
 
 			Console.ReadKey();
 		}
