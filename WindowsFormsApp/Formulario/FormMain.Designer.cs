4a108a49977e3d5c0d372d82ea2244dc285b966a
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-10-11 11:11:55 -0300

    :sparkles: feat: Criando menus para as Forms - Parte 1, aula 142

--- WindowsFormsApp/Formulario/FormMain.Designer.cs
+++ WindowsFormsApp/Formulario/FormMain.Designer.cs
@@ -31,14 +31,23 @@
 			this.lblTitulo = new System.Windows.Forms.Label();
 			this.btnSegundo = new System.Windows.Forms.Button();
 			this.btnSegundoFormThread = new System.Windows.Forms.Button();
+			this.menuBar = new System.Windows.Forms.MenuStrip();
+			this.menuFile = new System.Windows.Forms.ToolStripMenuItem();
+			this.mFileNovo = new System.Windows.Forms.ToolStripMenuItem();
+			this.mFileAbrir = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.mFileSair = new System.Windows.Forms.ToolStripMenuItem();
+			this.ajudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.sobreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuBar.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// lblTitulo
 			// 
 			this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lblTitulo.Location = new System.Drawing.Point(13, 13);
+			this.lblTitulo.Location = new System.Drawing.Point(16, 84);
 			this.lblTitulo.Name = "lblTitulo";
-			this.lblTitulo.Size = new System.Drawing.Size(775, 103);
+			this.lblTitulo.Size = new System.Drawing.Size(768, 103);
 			this.lblTitulo.TabIndex = 0;
 			this.lblTitulo.Text = "Principal";
 			this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -65,6 +74,66 @@
 			this.btnSegundoFormThread.UseVisualStyleBackColor = true;
 			this.btnSegundoFormThread.Click += new System.EventHandler(this.btnSegundoFormThread_Click);
 			// 
+			// menuBar
+			// 
+			this.menuBar.BackColor = System.Drawing.SystemColors.Control;
+			this.menuBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuFile,
+            this.ajudaToolStripMenuItem});
+			this.menuBar.Location = new System.Drawing.Point(0, 0);
+			this.menuBar.Name = "menuBar";
+			this.menuBar.Size = new System.Drawing.Size(800, 24);
+			this.menuBar.TabIndex = 3;
+			this.menuBar.Text = "menuStrip1";
+			// 
+			// menuFile
+			// 
+			this.menuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mFileNovo,
+            this.mFileAbrir,
+            this.toolStripSeparator1,
+            this.mFileSair});
+			this.menuFile.Name = "menuFile";
+			this.menuFile.Size = new System.Drawing.Size(37, 20);
+			this.menuFile.Text = "File";
+			// 
+			// mFileNovo
+			// 
+			this.mFileNovo.Name = "mFileNovo";
+			this.mFileNovo.Size = new System.Drawing.Size(180, 22);
+			this.mFileNovo.Text = "Novo";
+			// 
+			// mFileAbrir
+			// 
+			this.mFileAbrir.Name = "mFileAbrir";
+			this.mFileAbrir.Size = new System.Drawing.Size(180, 22);
+			this.mFileAbrir.Text = "Abrir";
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+			// 
+			// mFileSair
+			// 
+			this.mFileSair.Name = "mFileSair";
+			this.mFileSair.Size = new System.Drawing.Size(180, 22);
+			this.mFileSair.Text = "Sair";
+			// 
+			// ajudaToolStripMenuItem
+			// 
+			this.ajudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sobreToolStripMenuItem});
+			this.ajudaToolStripMenuItem.Name = "ajudaToolStripMenuItem";
+			this.ajudaToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+			this.ajudaToolStripMenuItem.Text = "Help";
+			// 
+			// sobreToolStripMenuItem
+			// 
+			this.sobreToolStripMenuItem.Name = "sobreToolStripMenuItem";
+			this.sobreToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.sobreToolStripMenuItem.Text = "Sobre";
+			// 
 			// FormMain
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -73,11 +142,16 @@
 			this.Controls.Add(this.btnSegundoFormThread);
 			this.Controls.Add(this.btnSegundo);
 			this.Controls.Add(this.lblTitulo);
+			this.Controls.Add(this.menuBar);
+			this.MainMenuStrip = this.menuBar;
 			this.Name = "FormMain";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Formulario Principal";
 			this.Load += new System.EventHandler(this.FormMain_Load);
+			this.menuBar.ResumeLayout(false);
+			this.menuBar.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -86,6 +160,14 @@
 		private System.Windows.Forms.Label lblTitulo;
 		private System.Windows.Forms.Button btnSegundo;
 		private System.Windows.Forms.Button btnSegundoFormThread;
+		private System.Windows.Forms.MenuStrip menuBar;
+		private System.Windows.Forms.ToolStripMenuItem menuFile;
+		private System.Windows.Forms.ToolStripMenuItem mFileNovo;
+		private System.Windows.Forms.ToolStripMenuItem mFileAbrir;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+		private System.Windows.Forms.ToolStripMenuItem mFileSair;
+		private System.Windows.Forms.ToolStripMenuItem ajudaToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem sobreToolStripMenuItem;
 	}
 }
 
