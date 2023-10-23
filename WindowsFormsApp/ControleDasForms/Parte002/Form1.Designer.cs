bc9d952e3cc5b47e0748c452139e412a0a03201f
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-10-23 11:12:13 -0300

    :sparkles: feat: Adicionando button - Comportamento dos Forms, aula 149

---
+++ WindowsFormsApp/ControleDasForms/Parte002/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Parte002
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnExecutar = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// btnExecutar
+			// 
+			this.btnExecutar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnExecutar.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+			this.btnExecutar.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnExecutar.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
+			this.btnExecutar.FlatAppearance.BorderSize = 2;
+			this.btnExecutar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+			this.btnExecutar.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.InactiveCaption;
+			this.btnExecutar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnExecutar.Font = new System.Drawing.Font("MV Boli", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnExecutar.ForeColor = System.Drawing.SystemColors.HotTrack;
+			this.btnExecutar.Location = new System.Drawing.Point(227, 155);
+			this.btnExecutar.Name = "btnExecutar";
+			this.btnExecutar.Size = new System.Drawing.Size(243, 68);
+			this.btnExecutar.TabIndex = 0;
+			this.btnExecutar.Text = "Executar";
+			this.btnExecutar.UseVisualStyleBackColor = false;
+			this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(696, 379);
+			this.Controls.Add(this.btnExecutar);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnExecutar;
+	}
+}
+
