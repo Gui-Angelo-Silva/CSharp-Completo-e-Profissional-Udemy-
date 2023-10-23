8488078502da74958e3728466080d3144cb8c1a2
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-10-23 08:55:28 -0300

    :sparkles: feat: Adicionando Formulário MdiContainer, aula 147

---
+++ WindowsFormsApp/FormContainer/FormTres.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace FormContainer
+{
+	partial class FormTres
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnDois = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// btnDois
+			// 
+			this.btnDois.Location = new System.Drawing.Point(292, 157);
+			this.btnDois.Name = "btnDois";
+			this.btnDois.Size = new System.Drawing.Size(177, 67);
+			this.btnDois.TabIndex = 0;
+			this.btnDois.Text = "Form Dois";
+			this.btnDois.UseVisualStyleBackColor = true;
+			this.btnDois.Click += new System.EventHandler(this.btnDois_Click);
+			// 
+			// FormTres
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.btnDois);
+			this.Name = "FormTres";
+			this.Text = "FormTres";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnDois;
+	}
+}
