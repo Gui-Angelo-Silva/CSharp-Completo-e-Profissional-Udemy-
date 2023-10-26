1b45e4156710b79a570bc9c7dea29374cb2d329c
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-10-26 08:42:30 -0300

    :spakles: feat: Adicionando TextBox - Controle das Forms, aula 150

---
+++ WindowsFormsApp/ControleDasForms/Parte003/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Parte003
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtMensagem = new System.Windows.Forms.TextBox();
+			this.lblResultado = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// txtMensagem
+			// 
+			this.txtMensagem.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtMensagem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+			this.txtMensagem.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.txtMensagem.Font = new System.Drawing.Font("Papyrus", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtMensagem.ForeColor = System.Drawing.SystemColors.HotTrack;
+			this.txtMensagem.Location = new System.Drawing.Point(150, 212);
+			this.txtMensagem.Name = "txtMensagem";
+			this.txtMensagem.Size = new System.Drawing.Size(500, 41);
+			this.txtMensagem.TabIndex = 0;
+			this.txtMensagem.TextChanged += new System.EventHandler(this.txtMensagem_TextChanged);
+			// 
+			// lblResultado
+			// 
+			this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblResultado.Location = new System.Drawing.Point(149, 32);
+			this.lblResultado.Name = "lblResultado";
+			this.lblResultado.Size = new System.Drawing.Size(503, 152);
+			this.lblResultado.TabIndex = 1;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.lblResultado);
+			this.Controls.Add(this.txtMensagem);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtMensagem;
+		private System.Windows.Forms.Label lblResultado;
+	}
+}
+
