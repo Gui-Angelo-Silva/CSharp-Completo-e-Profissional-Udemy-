6571e39860186101bb946acbac6e172da3c16ed8
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-10-27 11:03:57 -0300

    :spakles: feat: Adicionando o LinkLabel - Controle das Forms, aula 156

---
+++ WindowsFormsApp/ControleDasForms/Parte009/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Parte009
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+			this.SuspendLayout();
+			// 
+			// linkLabel1
+			// 
+			this.linkLabel1.AutoEllipsis = true;
+			this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.linkLabel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.linkLabel1.LinkArea = new System.Windows.Forms.LinkArea(0, 11);
+			this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+			this.linkLabel1.LinkColor = System.Drawing.Color.DarkRed;
+			this.linkLabel1.Location = new System.Drawing.Point(39, 42);
+			this.linkLabel1.Name = "linkLabel1";
+			this.linkLabel1.Size = new System.Drawing.Size(656, 38);
+			this.linkLabel1.TabIndex = 0;
+			this.linkLabel1.TabStop = true;
+			this.linkLabel1.Text = "Curso de C#";
+			this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.linkLabel1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.LinkLabel linkLabel1;
+	}
+}
+
