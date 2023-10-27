afd5712f3257fcfcc86f5ea43f49a7109ff71e15
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-10-27 11:33:31 -0300

    :spakles: feat: Adicionando o ListView - Controle das Forms, aula 157

---
+++ WindowsFormsApp/ControleDasForms/Parte010/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Parte010
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lista = new System.Windows.Forms.ListView();
+			this.colNome = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.colTelefone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.colEmail = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// lista
+			// 
+			this.lista.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colNome,
+            this.colTelefone,
+            this.colEmail});
+			this.lista.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lista.FullRowSelect = true;
+			this.lista.HideSelection = false;
+			this.lista.LabelEdit = true;
+			this.lista.Location = new System.Drawing.Point(12, 13);
+			this.lista.MultiSelect = false;
+			this.lista.Name = "lista";
+			this.lista.Size = new System.Drawing.Size(775, 389);
+			this.lista.TabIndex = 0;
+			this.lista.UseCompatibleStateImageBehavior = false;
+			this.lista.View = System.Windows.Forms.View.Details;
+			this.lista.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lista_MouseDoubleClick);
+			// 
+			// colNome
+			// 
+			this.colNome.Text = "Nome";
+			this.colNome.Width = 200;
+			// 
+			// colTelefone
+			// 
+			this.colTelefone.Text = "Telefone";
+			this.colTelefone.Width = 200;
+			// 
+			// colEmail
+			// 
+			this.colEmail.Text = "Email";
+			this.colEmail.Width = 250;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(588, 408);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(199, 63);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 485);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.lista);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListView lista;
+		private System.Windows.Forms.ColumnHeader colNome;
+		private System.Windows.Forms.ColumnHeader colTelefone;
+		private System.Windows.Forms.ColumnHeader colEmail;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ WindowsFormsApp/ControleDasForms/Parte010/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parte010
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+			ListViewItem item1 = new ListViewItem("Guilherme");
+			item1.SubItems.Add("17 99722-2467");
+			item1.SubItems.Add("guilherme@gmail.com");
+
+			ListViewItem item2 = new ListViewItem("Ana");
+			item1.SubItems.Add("17 99833-3578");
+			item1.SubItems.Add("ana@gmail.com");
+
+			ListViewItem item3 = new ListViewItem("Gabriel");
+			item1.SubItems.Add("17 99944-4689");
+			item1.SubItems.Add("gabriel@gmail.com");
+
+			ListViewItem item4 = new ListViewItem(new string[] { "Arthur", "17 99620-3389", "mail" });
+
+			lista.Items.Add(item1);
+			lista.Items.Add(item2);
+			lista.Items.Add(item3);
+			lista.Items.Add(item4);
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			//lista.Items.Clear();
+			//lista.Items.Remove(lista.SelectedItems[0]);
+			lista.Items.RemoveAt(1);
+		}
+
+		private void lista_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+
+		}
+	}
+}
