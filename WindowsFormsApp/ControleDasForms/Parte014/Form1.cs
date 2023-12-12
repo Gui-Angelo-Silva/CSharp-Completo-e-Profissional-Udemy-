264e2b86e6e7cd021f62a7ec9d6e9ca0f3cbe4fa
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-12-12 09:30:35 -0300

    :sparkles: feat: Controle das Forms - Adicionando PictureBox, aula 161

---
+++ WindowsFormsApp/ControleDasForms/Parte014/Form1.cs
@@ -0,0 +1,16 @@
+namespace Parte014
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			pictureBox1.BackgroundImage = Image.FromFile("");
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
