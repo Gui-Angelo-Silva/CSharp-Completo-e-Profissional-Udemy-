d0db3743ff799b2f586f83cf8e2fdfc175fe9ee7
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-12-27 08:57:53 -0300

    :sparkles: feat: Adicionando RichTextBox - Controle das Forms, aula 164

---
+++ WindowsFormsApp/ControleDasForms/Parte016/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Parte016
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.radio1 = new System.Windows.Forms.RadioButton();
+			this.SuspendLayout();
+			// 
+			// radio1
+			// 
+			this.radio1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.radio1.Location = new System.Drawing.Point(66, 76);
+			this.radio1.Name = "radio1";
+			this.radio1.Size = new System.Drawing.Size(283, 36);
+			this.radio1.TabIndex = 0;
+			this.radio1.Text = "Opção 1";
+			this.radio1.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.radio1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RadioButton radio1;
+	}
+}
+
---
+++ WindowsFormsApp/ControleDasForms/Parte017/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Parte017
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txt = new System.Windows.Forms.RichTextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// txt
+			// 
+			this.txt.BackColor = System.Drawing.SystemColors.Info;
+			this.txt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txt.Location = new System.Drawing.Point(13, 13);
+			this.txt.Name = "txt";
+			this.txt.Size = new System.Drawing.Size(775, 372);
+			this.txt.TabIndex = 0;
+			this.txt.Text = "";
+			this.txt.WordWrap = false;
+			this.txt.TextChanged += new System.EventHandler(this.txt_TextChanged);
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(13, 391);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 47);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "-";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(713, 391);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 47);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "+";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.txt);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RichTextBox txt;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+	}
+}
+
