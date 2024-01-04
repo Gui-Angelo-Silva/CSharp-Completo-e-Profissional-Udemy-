07bf10c23604d21a3687aa03a21c33d0e60b8d2c
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-01-04 09:25:43 -0300

    :sparkles: feat: Adicionando TreeView - Controle das Forms, aula 166

---
+++ WindowsFormsApp/ControleDasForms/Projeto019/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto019
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			MontarTree();
+		}
+
+		private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+		{
+			string node = treeView1.SelectedNode.Text;
+
+			MessageBox.Show(node);
+		}
+
+		private void MontarTree()
+		{
+			treeView1.BeginUpdate();
+
+			treeView1.Nodes.Add("Nomes");
+			treeView1.Nodes[0].Nodes.Add("Guilherme");
+			treeView1.Nodes[0].Nodes[0].Nodes.Add("Dados Pessoais");
+			treeView1.Nodes[0].Nodes[0].Nodes.Add("Dados Profissionais");
+
+			treeView1.Nodes[0].Nodes.Add("Gabriel");
+			treeView1.Nodes[0].Nodes.Add("Gustavo");
+
+			treeView1.EndUpdate();
+		}
+	}
+}
