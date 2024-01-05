d3b9c0dbeb891fdb30320f47a83a4fb1da10c489
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-01-05 09:43:09 -0300

    :sparkles: feat: Adicionando StatusTrip - Controle das Forms, aula 176

---
+++ WindowsFormsApp/ControleDasForms/Parte029/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Parte029
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.statusBarra = new System.Windows.Forms.StatusStrip();
+			this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+			this.statusProgress = new System.Windows.Forms.ToolStripProgressBar();
+			this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+			this.zoom100ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.zoomToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.zoomToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSplitButton();
+			this.novoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.statusBarra.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// statusBarra
+			// 
+			this.statusBarra.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel,
+            this.statusProgress,
+            this.toolStripDropDownButton1,
+            this.toolStripSplitButton1});
+			this.statusBarra.Location = new System.Drawing.Point(0, 428);
+			this.statusBarra.Name = "statusBarra";
+			this.statusBarra.Size = new System.Drawing.Size(800, 22);
+			this.statusBarra.TabIndex = 0;
+			this.statusBarra.Text = "statusStrip1";
+			// 
+			// statusLabel
+			// 
+			this.statusLabel.Name = "statusLabel";
+			this.statusLabel.Size = new System.Drawing.Size(35, 17);
+			this.statusLabel.Text = "100%";
+			// 
+			// statusProgress
+			// 
+			this.statusProgress.Name = "statusProgress";
+			this.statusProgress.Size = new System.Drawing.Size(100, 16);
+			// 
+			// toolStripDropDownButton1
+			// 
+			this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zoom100ToolStripMenuItem,
+            this.zoomToolStripMenuItem1,
+            this.zoomToolStripMenuItem});
+			this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+			this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+			this.toolStripDropDownButton1.Size = new System.Drawing.Size(29, 20);
+			this.toolStripDropDownButton1.Text = "toolStripDropDownButton1";
+			// 
+			// zoom100ToolStripMenuItem
+			// 
+			this.zoom100ToolStripMenuItem.Name = "zoom100ToolStripMenuItem";
+			this.zoom100ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.zoom100ToolStripMenuItem.Text = "Zoom 100%";
+			this.zoom100ToolStripMenuItem.Click += new System.EventHandler(this.zoom100ToolStripMenuItem_Click);
+			// 
+			// zoomToolStripMenuItem
+			// 
+			this.zoomToolStripMenuItem.Name = "zoomToolStripMenuItem";
+			this.zoomToolStripMenuItem.ShortcutKeyDisplayString = "Ctrl + \'-\'";
+			this.zoomToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.OemMinus)));
+			this.zoomToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.zoomToolStripMenuItem.Text = "Zoom -";
+			this.zoomToolStripMenuItem.Click += new System.EventHandler(this.zoomToolStripMenuItem_Click);
+			// 
+			// zoomToolStripMenuItem1
+			// 
+			this.zoomToolStripMenuItem1.Name = "zoomToolStripMenuItem1";
+			this.zoomToolStripMenuItem1.ShortcutKeyDisplayString = "Ctrl + \'+\'";
+			this.zoomToolStripMenuItem1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Oemplus)));
+			this.zoomToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+			this.zoomToolStripMenuItem1.Text = "Zoom +";
+			this.zoomToolStripMenuItem1.Click += new System.EventHandler(this.zoomToolStripMenuItem1_Click);
+			// 
+			// toolStripSplitButton1
+			// 
+			this.toolStripSplitButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripSplitButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.novoToolStripMenuItem});
+			this.toolStripSplitButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripSplitButton1.Image")));
+			this.toolStripSplitButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripSplitButton1.Name = "toolStripSplitButton1";
+			this.toolStripSplitButton1.Size = new System.Drawing.Size(32, 20);
+			this.toolStripSplitButton1.Text = "toolStripSplitButton1";
+			// 
+			// novoToolStripMenuItem
+			// 
+			this.novoToolStripMenuItem.Name = "novoToolStripMenuItem";
+			this.novoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.novoToolStripMenuItem.Text = "Novo";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.statusBarra);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.statusBarra.ResumeLayout(false);
+			this.statusBarra.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.StatusStrip statusBarra;
+		private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+		private System.Windows.Forms.ToolStripProgressBar statusProgress;
+		private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+		private System.Windows.Forms.ToolStripMenuItem zoom100ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem zoomToolStripMenuItem1;
+		private System.Windows.Forms.ToolStripMenuItem zoomToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton1;
+		private System.Windows.Forms.ToolStripMenuItem novoToolStripMenuItem;
+	}
+}
+
