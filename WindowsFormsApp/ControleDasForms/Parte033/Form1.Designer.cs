db6a7bc203888cfeab5bdf6caac64db928e4ad88
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-01-05 10:49:44 -0300

    :sparkles: feat: Adicionando Timer - Controle das Forms, aula 180

---
+++ WindowsFormsApp/ControleDasForms/Parte033/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Parte033
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.label1 = new System.Windows.Forms.Label();
+			this.btnStart = new System.Windows.Forms.Button();
+			this.btnStop = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 3000;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(132, 61);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(536, 134);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "0";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// btnStart
+			// 
+			this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnStart.Location = new System.Drawing.Point(139, 291);
+			this.btnStart.Name = "btnStart";
+			this.btnStart.Size = new System.Drawing.Size(204, 90);
+			this.btnStart.TabIndex = 1;
+			this.btnStart.Text = "Start";
+			this.btnStart.UseVisualStyleBackColor = true;
+			this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+			// 
+			// btnStop
+			// 
+			this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnStop.Location = new System.Drawing.Point(464, 291);
+			this.btnStop.Name = "btnStop";
+			this.btnStop.Size = new System.Drawing.Size(204, 90);
+			this.btnStop.TabIndex = 2;
+			this.btnStop.Text = "Stop";
+			this.btnStop.UseVisualStyleBackColor = true;
+			this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.btnStop);
+			this.Controls.Add(this.btnStart);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button btnStart;
+		private System.Windows.Forms.Button btnStop;
+	}
+}
+
