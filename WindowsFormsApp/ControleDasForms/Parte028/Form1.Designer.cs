b1c7bc25b7c470bbc9bdfaa11704e9b32eebe680
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-01-05 09:19:41 -0300

    :sparkles: feat: Adicionando MenuStrip - Controle das Forms, aula 175

---
+++ WindowsFormsApp/ControleDasForms/Parte028/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Parte028
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.menuMain = new System.Windows.Forms.MenuStrip();
+			this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.opçõesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.sibreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuMain.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuMain
+			// 
+			this.menuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem,
+            this.opçõesToolStripMenuItem});
+			this.menuMain.Location = new System.Drawing.Point(0, 0);
+			this.menuMain.Name = "menuMain";
+			this.menuMain.Size = new System.Drawing.Size(800, 24);
+			this.menuMain.TabIndex = 0;
+			this.menuMain.Text = "menuStrip1";
+			// 
+			// arquivoToolStripMenuItem
+			// 
+			this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.sairToolStripMenuItem});
+			this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+			this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+			this.arquivoToolStripMenuItem.Text = "Arquivo";
+			// 
+			// abrirToolStripMenuItem
+			// 
+			this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+			this.abrirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.abrirToolStripMenuItem.Text = "Abrir";
+			// 
+			// sairToolStripMenuItem
+			// 
+			this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+			this.sairToolStripMenuItem.ShortcutKeyDisplayString = "";
+			this.sairToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.Q)));
+			this.sairToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.sairToolStripMenuItem.Text = "Sair";
+			this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+			// 
+			// opçõesToolStripMenuItem
+			// 
+			this.opçõesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sibreToolStripMenuItem});
+			this.opçõesToolStripMenuItem.Name = "opçõesToolStripMenuItem";
+			this.opçõesToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+			this.opçõesToolStripMenuItem.Text = "Opções";
+			// 
+			// sibreToolStripMenuItem
+			// 
+			this.sibreToolStripMenuItem.Name = "sibreToolStripMenuItem";
+			this.sibreToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.sibreToolStripMenuItem.Text = "Sobre";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.menuMain);
+			this.MainMenuStrip = this.menuMain;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.menuMain.ResumeLayout(false);
+			this.menuMain.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuMain;
+		private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem opçõesToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem sibreToolStripMenuItem;
+	}
+}
+
