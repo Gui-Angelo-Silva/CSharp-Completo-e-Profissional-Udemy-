77baf3de4701f184177fdec2ce42abe803b276f3
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-01-18 09:07:44 -0300

    :sparkles: feat: Adicionando FontDialog - Controle das Forms, aula 188

---
+++ WindowsFormsApp/ControleDasForms/Parte041/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Parte041
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.font = new System.Windows.Forms.FontDialog();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// font
+			// 
+			this.font.ShowApply = true;
+			this.font.ShowColor = true;
+			this.font.Apply += new System.EventHandler(this.font_Apply);
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(63, 90);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(674, 246);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Clique e selecione uma fonte...";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.FontDialog font;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
