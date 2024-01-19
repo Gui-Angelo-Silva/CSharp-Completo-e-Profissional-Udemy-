d54ccb7b6da1baec5f65d5db55114959a5ea30eb
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-01-19 11:15:14 -0300

    :sparkles: feat: Adicionando Classe Directory - Manipular Arquivos e Pastas, aula 195

---
+++ WindowsFormsApp/Arquivo/Diretorio/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Diretorio
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lista = new System.Windows.Forms.ListBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// lista
+			// 
+			this.lista.FormattingEnabled = true;
+			this.lista.Location = new System.Drawing.Point(13, 13);
+			this.lista.Name = "lista";
+			this.lista.Size = new System.Drawing.Size(775, 342);
+			this.lista.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(313, 373);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(175, 62);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Executar";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.lista);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox lista;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ WindowsFormsApp/Arquivo/Diretorio/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Diretorio
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string path = @"c:\curso\";
+
+			bool res = Directory.Exists(path + "guigas");
+			//lista.Items.Add(res);
+
+			//if (!res)
+			//{
+			//	Directory.CreateDirectory(path + "guigas");
+			//}
+
+			//if (res)
+			//{
+			//	Directory.Delete(path + "guigas");
+			//}
+
+			//if (res)
+			//{
+			//	Directory.Move(path + "guigas", path + @"destino\guigas");
+			//}
+
+			//string[] dirs = Directory.GetDirectories(path);
+
+			//foreach (string d in dirs)
+			//{
+			//	lista.Items.Add(d);
+			//}
+
+			//string[] files = Directory.GetFiles(path);
+
+			//foreach (string f in files)
+			//{
+			//	lista.Items.Add(f);
+			//}
+
+			//string root = Directory.GetDirectoryRoot(path);
+			//lista.Items.Add(root);
+
+			//string[] drives = Directory.GetLogicalDrives();
+			//lista.Items.AddRange(drives);
+
+			//DirectoryInfo info = Directory.GetParent(path);
+			//lista.Items.Add(info.FullName);
+
+			string dir = Directory.GetCurrentDirectory();
+			lista.Items.Add(dir);
+        }
+	}
+}
