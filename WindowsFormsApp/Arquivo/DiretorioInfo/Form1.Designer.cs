2fa7534831bf3fba6aec4550e1b1d165765bff08
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-01-19 11:25:11 -0300

    :sparkles: feat: Adicionando Classe DirectoryInfo - Manipular Arquivos e Pastas, aula 196

--- WindowsFormsApp/Arquivo/DiretorioInfo/Form1.Designer.cs
+++ WindowsFormsApp/Arquivo/DiretorioInfo/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Diretorio
+﻿namespace DiretorioInfo
 {
 	partial class Form1
 	{
@@ -29,36 +29,36 @@
 		private void InitializeComponent()
 		{
 			this.lista = new System.Windows.Forms.ListBox();
-			this.button1 = new System.Windows.Forms.Button();
+			this.btnExecutar = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// lista
 			// 
-			this.lista.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lista.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.lista.FormattingEnabled = true;
-			this.lista.ItemHeight = 24;
+			this.lista.ItemHeight = 20;
 			this.lista.Location = new System.Drawing.Point(13, 13);
 			this.lista.Name = "lista";
-			this.lista.Size = new System.Drawing.Size(775, 340);
+			this.lista.Size = new System.Drawing.Size(775, 344);
 			this.lista.TabIndex = 0;
 			// 
-			// button1
+			// btnExecutar
 			// 
-			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button1.Location = new System.Drawing.Point(313, 373);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(175, 62);
-			this.button1.TabIndex = 1;
-			this.button1.Text = "Executar";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
+			this.btnExecutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnExecutar.Location = new System.Drawing.Point(290, 374);
+			this.btnExecutar.Name = "btnExecutar";
+			this.btnExecutar.Size = new System.Drawing.Size(221, 64);
+			this.btnExecutar.TabIndex = 1;
+			this.btnExecutar.Text = "Executar";
+			this.btnExecutar.UseVisualStyleBackColor = true;
+			this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.button1);
+			this.Controls.Add(this.btnExecutar);
 			this.Controls.Add(this.lista);
 			this.Name = "Form1";
 			this.Text = "Form1";
@@ -69,7 +69,7 @@
 		#endregion
 
 		private System.Windows.Forms.ListBox lista;
-		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button btnExecutar;
 	}
 }
 
