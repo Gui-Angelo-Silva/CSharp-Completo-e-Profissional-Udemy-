1fa2faf8578655ab065f10dbc61b5cc67025cf11
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-02-09 08:19:13 -0300

    :sparkles: feat: Adicionando Escrita de Arquivos de Texto - Manipular arquivos e pastas, aula 197

---
+++ WindowsFormsApp/Arquivo/LerEscrever/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace LerEscrever
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtConteudo = new System.Windows.Forms.TextBox();
+			this.btnEscreverTxt = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// txtConteudo
+			// 
+			this.txtConteudo.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtConteudo.Location = new System.Drawing.Point(13, 13);
+			this.txtConteudo.Multiline = true;
+			this.txtConteudo.Name = "txtConteudo";
+			this.txtConteudo.Size = new System.Drawing.Size(775, 350);
+			this.txtConteudo.TabIndex = 0;
+			// 
+			// btnEscreverTxt
+			// 
+			this.btnEscreverTxt.Location = new System.Drawing.Point(13, 386);
+			this.btnEscreverTxt.Name = "btnEscreverTxt";
+			this.btnEscreverTxt.Size = new System.Drawing.Size(169, 52);
+			this.btnEscreverTxt.TabIndex = 1;
+			this.btnEscreverTxt.Text = "Escrever Txt";
+			this.btnEscreverTxt.UseVisualStyleBackColor = true;
+			this.btnEscreverTxt.Click += new System.EventHandler(this.btnEscreverTxt_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.btnEscreverTxt);
+			this.Controls.Add(this.txtConteudo);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtConteudo;
+		private System.Windows.Forms.Button btnEscreverTxt;
+	}
+}
+
---
+++ WindowsFormsApp/Arquivo/LerEscrever/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LerEscrever
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnEscreverTxt_Click(object sender, EventArgs e)
+		{
+			string path = @"c:\teste\file.txt";
+
+			StreamWriter writer = new StreamWriter(path, true, Encoding.Default);
+
+			//string linha = txtConteudo.Text;
+			//writer.WriteLine(linha);
+
+			string txt = txtConteudo.Text;
+			writer.Write(txt);
+
+			//writer.Flush();
+			//writer.Dispose();
+			writer.Close();
+		}
+	}
+}
