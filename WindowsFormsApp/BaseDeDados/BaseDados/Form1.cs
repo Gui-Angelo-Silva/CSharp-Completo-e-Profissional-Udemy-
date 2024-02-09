ad4f6f1231c13f9490cd0b602a1649a1a39b8546
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-02-09 10:30:24 -0300

    :sparkles: feat: Adicionando Preparando Projeto para SQLite - Trabalhar com bases de dados, aula 203

--- WindowsFormsApp/BaseDeDados/BaseDados/Form1.cs
+++ WindowsFormsApp/BaseDeDados/BaseDados/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
 
 namespace BaseDados
 {
@@ -44,7 +46,31 @@ namespace BaseDados
 
 		private void btnConectar_Click(object sender, EventArgs e)
 		{
+			#region SQLite
+			string baseDados = Application.StartupPath + @"\db\DBSQLite.sdf";
+			string strConnection = @"DataSource = " + baseDados + "; Version = 3";
 
-		}
+            if (!File.Exists(baseDados))
+            {
+				SQLiteConnection.CreateFile(baseDados);
+            }
+
+			SQLiteConnection conexao = new SQLiteConnection(strConnection);
+
+			try
+			{
+				conexao.Open();
+				lblResultado.Text = "Conectado ao SQLite";
+			}
+			catch (Exception ex)
+			{
+				lblResultado.Text = "Erro ao Conectar SQLite \n" + ex;
+			}
+			finally
+			{
+				conexao.Close();
+			}
+            #endregion
+        }
 	}
 }
