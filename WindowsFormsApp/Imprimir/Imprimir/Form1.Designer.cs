bb25d530d545f3737c1ed4141adfc112c1c96f16
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-03-25 08:59:29 -0300

    :sparkles: feat: Adicionando Impressão - Trabalhar com Impressão, aula 229

---
+++ WindowsFormsApp/Imprimir/Imprimir/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Imprimir
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+			this.btnImprimir = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// printDocument1
+			// 
+			this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+			// 
+			// btnImprimir
+			// 
+			this.btnImprimir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnImprimir.Location = new System.Drawing.Point(595, 377);
+			this.btnImprimir.Name = "btnImprimir";
+			this.btnImprimir.Size = new System.Drawing.Size(193, 61);
+			this.btnImprimir.TabIndex = 0;
+			this.btnImprimir.Text = "Imprimir";
+			this.btnImprimir.UseVisualStyleBackColor = true;
+			this.btnImprimir.Click += new System.EventHandler(this.btnImprimir_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.btnImprimir);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Drawing.Printing.PrintDocument printDocument1;
+		private System.Windows.Forms.Button btnImprimir;
+	}
+}
+
---
+++ WindowsFormsApp/Imprimir/Imprimir/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Imprimir
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnImprimir_Click(object sender, EventArgs e)
+		{
+			printDocument1.Print();
+		}
+
+		private void printDocument1_PrintPage(object sender, PrintPageEventArgs e)
+		{
+			#region Parte 1
+
+			string texto = "Trabalhando com impressão.";
+			Font letra = new Font("Arial", 20, FontStyle.Bold, GraphicsUnit.Point);
+			Brush pincel = new SolidBrush(Color.Black);
+			Point pontoInicial = new Point(100, 50);
+
+			// Desenhar o documento para ser impresso
+			e.Graphics.DrawString(texto, letra, pincel, pontoInicial);
+			e.Graphics.DrawString("Guilherme Silva", letra, Brushes.Red, new Point(150, 150));
+			e.Graphics.FillRectangle(Brushes.Green, new Rectangle(100, 220, 400, 100));
+
+			#endregion
+		}
+	}
+}
