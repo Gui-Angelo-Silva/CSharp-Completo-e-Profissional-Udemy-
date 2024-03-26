e8c623a3cc3e6ab3ea08e0f9517ffd5dd112bc7c
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-03-26 08:51:09 -0300

    :sparkles: feat: Adicionando Formatação do Documento de Impressão - Trabalhar com Impressão, aula 230

--- WindowsFormsApp/Imprimir/Imprimir/Form1.cs
+++ WindowsFormsApp/Imprimir/Imprimir/Form1.cs
@@ -27,15 +27,43 @@ namespace Imprimir
 		{
 			#region Parte 1
 
-			string texto = "Trabalhando com impressão.";
-			Font letra = new Font("Arial", 20, FontStyle.Bold, GraphicsUnit.Point);
+			//string texto = "Trabalhando com impressão.";
+			//Font letra = new Font("Arial", 20, FontStyle.Bold, GraphicsUnit.Point);
+			//Brush pincel = new SolidBrush(Color.Black);
+			//Point pontoInicial = new Point(100, 50);
+
+			//// Desenhar o documento para ser impresso
+			//e.Graphics.DrawString(texto, letra, pincel, pontoInicial);
+			//e.Graphics.DrawString("Guilherme Silva", letra, Brushes.Red, new Point(150, 150));
+			//e.Graphics.FillRectangle(Brushes.Green, new Rectangle(100, 220, 400, 100));
+
+			#endregion
+
+			#region Parte 2
+
+			int x = printDocument1.DefaultPageSettings.Bounds.X;
+			int y = printDocument1.DefaultPageSettings.Bounds.Y;
+			int altura = printDocument1.DefaultPageSettings.Bounds.Height;
+			int largura = printDocument1.DefaultPageSettings.Bounds.Width;
+
+			string titulo = "Titulo da Página";
+			Font letraTitulo = new Font("Verdana", 24, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point);
+			Brush pincelTitutlo = new SolidBrush(Color.Red);
+			Rectangle areaTitulo = new Rectangle(x + 50, y + 100, largura - 100, 100);
+			StringFormat formatoTitulo = new StringFormat();
+			formatoTitulo.Alignment = StringAlignment.Center;
+			formatoTitulo.LineAlignment = StringAlignment.Center;
+
+			string texto = "Trabalhando com impressão.Trabalhando com impressão.Trabalhando com impressão.Trabalhando com impressão.Trabalhando com impressão.Trabalhando com impressão.Trabalhando com impressão.";
+			Font letra = new Font("Arial", 18, FontStyle.Bold, GraphicsUnit.Point);
 			Brush pincel = new SolidBrush(Color.Black);
-			Point pontoInicial = new Point(100, 50);
+			Rectangle areaTexto = new Rectangle(x + 50, y + 200, largura - 100, altura - 400);	
 
 			// Desenhar o documento para ser impresso
-			e.Graphics.DrawString(texto, letra, pincel, pontoInicial);
-			e.Graphics.DrawString("Guilherme Silva", letra, Brushes.Red, new Point(150, 150));
-			e.Graphics.FillRectangle(Brushes.Green, new Rectangle(100, 220, 400, 100));
+			e.Graphics.DrawString(titulo, letraTitulo, pincelTitutlo, areaTitulo, formatoTitulo);
+			e.Graphics.DrawString(texto, letra, pincel, areaTexto);
+			//e.Graphics.DrawRectangle(new Pen(Color.Red, 5), areaTitulo);
+			//e.Graphics.DrawRectangle(new Pen(Color.Red, 5), areaTexto);
 
 			#endregion
 		}
