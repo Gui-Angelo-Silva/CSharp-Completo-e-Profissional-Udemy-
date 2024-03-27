cb3b2a877610a8a3d53da87e357c7ae0de2ef3be
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-03-27 10:02:41 -0300

    :sparkles: feat: Adicionando Sockets - Trabalhar com Sockets Cliente - Servidor, aula 233

---
+++ WindowsFormsApp/SocketClient/SocketClient/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+			try
+			{
+				IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+			
+				socket.Connect(endPoint);
+
+				Console.WriteLine("Conectado com exito...");
+				Console.WriteLine("Insira a informação para enviar: ");
+				Console.WriteLine();
+
+				string info = Console.ReadLine();
+
+				byte[] infoEnviar = Encoding.UTF8.GetBytes(info);
+
+				socket.Send(infoEnviar, 0, infoEnviar.Length, SocketFlags.None);
+			}
+			catch (Exception)
+			{
+				Console.WriteLine("Não foi possível conectar ao servidor...");
+			}
+
+			socket.Close();
+
+			Console.WriteLine("Servidor Finalizado\nPressione qualquer tecla para finalizar...");
+			Console.ReadKey();
+		}
+	}
+}
