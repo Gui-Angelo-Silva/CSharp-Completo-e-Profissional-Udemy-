eff9a97db01355430da05bedd3f4374b762f2bf2
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-04-03 20:09:26 -0300

    :sparkles: feat: Adicionando Instalador da Aplicação - Tópicos Complementares, aula 242

---
+++ WindowsFormsApp/TopicosComplementares/MeuApp/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace MeuApp
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lblMensagem = new System.Windows.Forms.Label();
+			this.btnExecutar = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// lblMensagem
+			// 
+			this.lblMensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblMensagem.Location = new System.Drawing.Point(13, 13);
+			this.lblMensagem.Name = "lblMensagem";
+			this.lblMensagem.Size = new System.Drawing.Size(764, 264);
+			this.lblMensagem.TabIndex = 0;
+			this.lblMensagem.Text = "Mensagem";
+			this.lblMensagem.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// btnExecutar
+			// 
+			this.btnExecutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnExecutar.Location = new System.Drawing.Point(289, 343);
+			this.btnExecutar.Name = "btnExecutar";
+			this.btnExecutar.Size = new System.Drawing.Size(225, 59);
+			this.btnExecutar.TabIndex = 1;
+			this.btnExecutar.Text = "Executar";
+			this.btnExecutar.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.lblMensagem);
+			this.Controls.Add(this.btnExecutar);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label lblMensagem;
+		private System.Windows.Forms.Button btnExecutar;
+	}
+}
+
